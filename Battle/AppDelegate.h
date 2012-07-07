9837013c4c384e49b75f02041babab28bda1900c
U-no <ueno@MACMINI.local>
2012-07-07 20:02:38 +0900

    Initial Commit

---
+++ Battle/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Battle
+//
+//  Created by Ueno Masahiro on 12/07/07.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Battle/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Battle
+//
+//  Created by Ueno Masahiro on 12/07/07.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
